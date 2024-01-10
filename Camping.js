8fbdaf5181d6f1bf8eb63d93b4aaa2e972aef00e
ahyng <ahyng1@gmail.com>
2024-01-10 17:11:38 +0900

    add Camping page

---
+++ Camping.js
@@ -0,0 +1,9 @@
+function button() {
+    document.getElementById('backbutton').style.backgroundColor="rgba(211, 211, 234, 0.729)";
+    document.getElementById('backbutton').style.color="black";
+}
+
+function button_out() {
+    document.getElementById('backbutton').style.backgroundColor="rgba(240, 248, 255, 0.071)";
+    document.getElementById('backbutton').style.color="white";
+}
